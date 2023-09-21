40ba263b9359820664717d4d4d7a831c85fed3b8
Nicholas <nick.nogue28@outlook.com>
2023-09-21 18:06:57 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/nichol20/gopportunities/router"
+
+func main() {
+	router.Initialize()
+}
---
+++ router/router.go
@@ -0,0 +1,10 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	// Initialize Router
+	router := gin.Default()
+	InitializeRoutes(router)
+	router.Run(":8080") //listen and serve on 0.0.0.0:8080
+}
